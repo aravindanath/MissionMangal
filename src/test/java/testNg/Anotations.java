5d4dfa962e1e354cd82e0766bd8d438bc189b4c1
aravindanath <aravindanath86@gmail.com>
2019-10-12 17:31:07 +0530

    my second commit

--- src/test/java/testNg/Anotations.java
+++ src/test/java/testNg/Anotations.java
@@ -9,6 +9,10 @@ public class Anotations {
 	public void login() {
 		System.out.println("My first test");
 	}
+	@Test
+	public void logout() {
+		System.out.println("My first test logout");
+	}
 	
 
 }
